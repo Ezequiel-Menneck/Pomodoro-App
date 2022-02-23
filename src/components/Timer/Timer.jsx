bdd5385ab64ef525150da9e3a9ce22d6b038b129
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-02-23 10:55:26 -0300

    feat: start timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import "./Timer.css";
 
 const Timer = (props) => {
+    const [min, setMin] = useState('25');
+    const [sec, setSec] = useState('00');
+    
+
+    const start = () => {
+        setSec(59)
+        setInterval(() => {       
+            setSec(sec => sec - 1) 
+        }, 1000);
+    }
+    
+    useEffect(() => {
+        if(sec == -1) {
+            setMin(min => min - 1)
+        }
+        if(sec == -1) {
+            setSec(59)
+        }
+    }, [sec])
+
     return (
         <div className="timer-main">
-            <div className='timer-types'>
+            <div className="timer-types">
                 <div className="timer-types__buttons">
                     <button className="button-type">Pomodoro</button>
                     <button className="button-type">Short Break</button>
                     <button className="button-type">Long Break</button>
                 </div>
-                <div className="timer">25:00</div>
+                <div className="timer">{min}:{sec}</div>
             </div>
             <div className="timer-start">
-                <button className='start'>
+                <button className="start" onClick={() => start()}>
                     START
                 </button>
             </div>
