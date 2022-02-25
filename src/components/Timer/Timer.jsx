c181c56d7502f21a73b28a9d9bc77b0392839abc
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-02-24 23:51:46 -0300

    timer improvement

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,25 +2,36 @@ import { useEffect, useState } from "react";
 import "./Timer.css";
 
 const Timer = (props) => {
-    const [min, setMin] = useState('25');
-    const [sec, setSec] = useState('00');
+    const [min, setMin] = useState("25");
+    const [sec, setSec] = useState("00");
+    const [intervalId, setIntervalId] = useState(0)
     
-
     const start = () => {
-        setSec(59)
-        setInterval(() => {       
-            setSec(sec => sec - 1) 
-        }, 1000);
-    }
-    
+        setMin(24);
+        setSec(59);
+        const newIntervalId = setInterval(() => {
+            setSec(sec < 10 ? (sec) => sec - 1 : sec + 1);        
+        }, 1000)
+        setIntervalId(newIntervalId)
+    };
+
     useEffect(() => {
-        if(sec == -1) {
-            setMin(min => min - 1)
-        }
-        if(sec == -1) {
-            setSec(59)
-        }
+        
     }, [sec])
+
+    useEffect(() => {
+        if (min == 0 && sec == 0) {
+            clearInterval(intervalId)
+            setSec('00')
+            setMin('25')
+        }
+        if (sec == -1) {
+            setMin((min) => min - 1);
+        }
+        if (sec == -1) {
+            setSec(59);
+        }
+    }, [sec]);
 
     return (
         <div className="timer-main">
@@ -30,7 +41,9 @@ const Timer = (props) => {
                     <button className="button-type">Short Break</button>
                     <button className="button-type">Long Break</button>
                 </div>
-                <div className="timer">{min}:{sec}</div>
+                <div className="timer">
+                    {min}:{sec}
+                </div>
             </div>
             <div className="timer-start">
                 <button className="start" onClick={() => start()}>
