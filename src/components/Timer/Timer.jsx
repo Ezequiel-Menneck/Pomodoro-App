bbc940c33d88afc89e4cb7d30579a8788533dde3
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-03-02 08:29:19 -0300

    improvements in Timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,37 +1,25 @@
 import { useEffect, useState } from "react";
 import "./Timer.css";
 
+const COUNTDOWN_TIME_SECONDS =  25 * 60; // 25 minutos em segundos
+
 const Timer = (props) => {
-    const [min, setMin] = useState("25");
-    const [sec, setSec] = useState("00");
-    const [intervalId, setIntervalId] = useState(0)
-    
+    const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_TIME_SECONDS);
+    const [intervalId, setIntervalId] = useState(0);
+
+    const minutes = Math.floor(secondsAmount / 60);
+    const seconds = secondsAmount % 60;
+
     const start = () => {
-        setMin(24);
-        setSec(59);
         const newIntervalId = setInterval(() => {
-            setSec(sec < 10 ? (sec) => sec - 1 : sec + 1);        
-        }, 1000)
-        setIntervalId(newIntervalId)
+            setSecondsAmount((sec) => sec - 1);
+        }, 1000);
+        setIntervalId(newIntervalId);
     };
 
     useEffect(() => {
-        
-    }, [sec])
-
-    useEffect(() => {
-        if (min == 0 && sec == 0) {
-            clearInterval(intervalId)
-            setSec('00')
-            setMin('25')
-        }
-        if (sec == -1) {
-            setMin((min) => min - 1);
-        }
-        if (sec == -1) {
-            setSec(59);
-        }
-    }, [sec]);
+        if(minutes === 0 && seconds === 0) clearInterval(intervalId)
+    }, [secondsAmount])
 
     return (
         <div className="timer-main">
@@ -42,7 +30,9 @@ const Timer = (props) => {
                     <button className="button-type">Long Break</button>
                 </div>
                 <div className="timer">
-                    {min}:{sec}
+                    <span>{String(minutes).padStart(2, "0")}</span>
+                    <span>:</span>
+                    <span>{String(seconds).padStart(2, "0")}</span>
                 </div>
             </div>
             <div className="timer-start">
