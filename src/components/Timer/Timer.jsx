fe0cee58e53d0bf85f7e16e5c45a30e1c40baf44
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-03-09 00:02:51 -0300

    add new improvements in timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 import "./Timer.css";
 
-const COUNTDOWN_TIME_SECONDS =  25 * 60; // 25 minutos em segundos
+const COUNTDOWN_TIME_SECONDS = 5; // 25 minutos em segundos
 
 const Timer = (props) => {
     const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_TIME_SECONDS);
     const [intervalId, setIntervalId] = useState(0);
+    const [running, setRunning] = useState(0)
 
     const minutes = Math.floor(secondsAmount / 60);
     const seconds = secondsAmount % 60;
 
     const start = () => {
+        setRunning(1)
         const newIntervalId = setInterval(() => {
             setSecondsAmount((sec) => sec - 1);
         }, 1000);
         setIntervalId(newIntervalId);
     };
 
+    const pause = () => {
+        setRunning(0)
+        clearInterval(intervalId)
+    }
+
     useEffect(() => {
-        if(minutes === 0 && seconds === 0) clearInterval(intervalId)
-    }, [secondsAmount])
+        if (minutes === 0 && seconds === 0) {
+            clearInterval(intervalId);
+            setSecondsAmount(COUNTDOWN_TIME_SECONDS)
+            setRunning(0)
+        }
+        console.log(secondsAmount, minutes, seconds)
+    }, [secondsAmount]);
 
     return (
         <div className="timer-main">
@@ -36,8 +48,8 @@ const Timer = (props) => {
                 </div>
             </div>
             <div className="timer-start">
-                <button className="start" onClick={() => start()}>
-                    START
+                <button className="start" onClick={() => running ? pause() : start()}>
+                    {running ? "PAUSE" : "START"}
                 </button>
             </div>
         </div>
