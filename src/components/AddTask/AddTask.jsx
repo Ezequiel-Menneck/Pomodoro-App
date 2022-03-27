294d32481eef2efa402b073639f9e21d8b84d45f
Ezequiel Menneck <zeeeee.peeeeetryyy@gmail.com>
2022-03-27 18:40:56 -0300

    New component and conditional render

---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,74 @@
+import "./AddTask.css";
+import { useState } from "react";
+
+const AddTask = (props) => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    return (
+        <>
+            {isVisible ? (
+                <button
+                    className="addTask-main"
+                    onClick={() => setIsVisible(false)}
+                >
+                    <img
+                        src="src\medias\plus-circle-white.png"
+                        alt="Add"
+                        className="addTask-img"
+                    />
+                    <span className="addTask-text">Add Task</span>
+                </button>
+            ) : (
+                <>
+                    <div className="addTask-form">
+                        <div className="addTask-work">
+                            <input
+                                type="text"
+                                placeholder="What are you working on?"
+                                className="addTask-input"
+                            />
+                            <p className="addTask-text2">Est Pomodoros</p>
+                        </div>
+                        <div className="addTask-formCount">
+                            <input
+                                type="number"
+                                value="1"
+                                className="addTask-inputCount"
+                            />
+                            <button className="addForm-button">
+                                <img
+                                    src="src\medias\caret-up.png"
+                                    alt="Up"
+                                    className="addForm-img"
+                                />
+                            </button>
+                            <button className="addForm-button">
+                                <img
+                                    className="addForm-img"
+                                    src="src\medias\caret-down.png"
+                                    alt="Down"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="addTask-buttons">
+                        <button
+                            className="addTask-button-cancel"
+                            onClick={() => setIsVisible(true)}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="addTask-button-save"
+                            onClick={() => setIsVisible(true)}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
+
+export default AddTask;
